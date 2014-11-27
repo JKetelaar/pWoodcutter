5daf782665b52d0ca6280c77c1ce1e0b468b7288
pbparachute <jelle@dutchrastacraft.nl>
2014-11-27 16:35:33 +0100

    Create Data.java

---
+++ src/parachute/scripts/pwoodcutter/main/Data.java
@@ -0,0 +1,25 @@
+package parachute.scripts.pwoodcutter.main;
+
+import org.rev317.min.api.wrappers.Tile;
+
+import parachute.scripts.api.Region;
+
+public class Data {
+	
+	//Objects
+	public static int idwillow[] = {1308, 5553, 5551, 5552};
+	
+	//Items
+	public static int hatchetsid[] = {1352, 1350, 1354, 1356, 1358, 1360, 1362, 6740};
+	
+	//groundItems
+	public static int nestids[] = {5070, 5071, 5072, 5073, 5074, 5075};
+	
+	//ChatInterfaces
+	public static int chatboxid = 968;
+	public static int chatboxfullid = 356;
+
+	//Regions
+	public static Region bankRegionDraynor = new Region(new Tile(3087, 3247), new Tile(3098, 3239));
+	
+}
